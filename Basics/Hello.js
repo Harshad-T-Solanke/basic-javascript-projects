1132364767342d9c65e739ff09f1d6e38b2113e1
Delta Student <harshadsolanke7789@gmail.com>
2026-09-07 00:21:00 +0530

    Add account details logging to Hello.js

--- Basics/Hello.js
+++ Basics/Hello.js
@@ -1,1 +1,16 @@
 console.log("Hello World")
+
+const accountId = 12345;
+let accountEmail = "harsh@gmail.com";
+var accountPassword = "4344"
+ accountCity = "pune";
+
+accountEmail = "har@gmail.com"
+accountCity = "mumbai"
+accountPassword = "212121"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountCity, accountPassword]);
+
+
